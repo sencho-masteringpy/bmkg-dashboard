959549d1238ebd46770e5da35f54c0baab41246b
sencho <senchoparameswara@gmail.com>
2022-02-12 12:35:27 +0700

    new code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bmkgdashboard",
-    version="0.1.3",
+    version="0.1.5",
     author="Sencho Parameswara",
     author_email="senchoparameswara@gmail.com",
     description="this package is dashboard for BMKG indonesian earthquake live and last articlee",
@@ -21,6 +21,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta"
     ],
+    package_dir={"earthquake": "earthquake", "article": "article"},
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
